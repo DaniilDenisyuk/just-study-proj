d48cd4086449c723262c37762ad848ce24cb2fdf
Daniil Denisyuk <ordekonec@gmail.com>
2020-06-05 20:43:43 +0300

    added route handlers (rough code)

--- test1/all.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-require('./unit.js');
-require('./system.js');
--- test1/unit.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const tests = ['config', 'logger', 'database', 'security', 'semaphore'];
-
-for (const name of tests) {
-  console.log(`Test unit: ${name}`);
-  require(`./unit.${name}.js`);
-}
