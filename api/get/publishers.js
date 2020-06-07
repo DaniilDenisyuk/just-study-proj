f5f99d53946484fc94aa4908cff7c8d9be1cfb36
Daniil Denisyuk <ordekonec@gmail.com>
2020-06-07 21:23:34 +0300

    route handlers added

--- api/get/publishers.js
+++ api/get/publishers.js
@@ -6,10 +6,18 @@ module.exports = async (pool, id = null, offset = 0, limit = 10) => {
       'from publisher inner join book on publisher.id = book.publisherid ' +
       'and publisher.id=$1 limit $2 offset $3', [id, limit, offset]).rows;
   } else {
-    data = await pool.query('select book.title,' +
-      ' book.published as published,' +
-      ' book.price from publisher limit $1 offset $2',
-    [limit, offset]).rows;
+    data = await new Promise((resolve, reject) => {
+      pool.query('select name,' +
+        ' address' +
+        ' from publisher limit $1 offset $2',
+      [limit, offset], (err, res) => {
+        if (err) {
+          throw err;
+        }
+        console.log(res.rows);
+        resolve(res.rows);
+      });
+    });
   }
   console.log(data);
   return data;
--- test/test.js
+++ test/test.js
@@ -12,7 +12,31 @@ const Config = require('../lib/config');
   const server2 = new Server(2);
   const server3 = new Server(3);
   const serverUrl = new URL('/api/authors', 'http://127.0.0.1:8082');
+  const serverUrl2 = new URL('/api/books', 'http://127.0.0.1:8081');
+  const serverUrl3 = new URL('/api/publishers', 'http://127.0.0.1:8083');
   http.get(serverUrl, resp => {
+    let data = '';
+    resp.on('data', chunk => {
+      data += chunk;
+    });
+    resp.on('end', () => {
+      console.log(data);
+    });
+  }).on('error', err => {
+    console.log('Error: ' + err.message);
+  });
+  http.get(serverUrl3, resp => {
+    let data = '';
+    resp.on('data', chunk => {
+      data += chunk;
+    });
+    resp.on('end', () => {
+      console.log(data);
+    });
+  }).on('error', err => {
+    console.log('Error: ' + err.message);
+  });
+  http.get(serverUrl2, resp => {
     let data = '';
     resp.on('data', chunk => {
       data += chunk;
