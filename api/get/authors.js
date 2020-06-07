844c6ad5014b8502f6b45a86822bc6392dad5968
Daniil Denisyuk <ordekonec@gmail.com>
2020-06-07 17:57:26 +0300

    preparation

--- api/get/authors.js
+++ api/get/authors.js
@@ -8,10 +8,18 @@ module.exports = async (pool, id, offset, limit) => {
       ' book.price from author inner join book on author.id = book.authorid ' +
       'and author.id=$1 limit $2 offset $3', [id, limit, offset]).rows;
   } else {
-    data = await pool.query('select book.title,' +
-      ' book.published as published,' +
-      ' book.price from author limit $1 offset $2',
-    [limit, offset]).rows;
+    data = await new Promise((resolve, reject) =>  {
+      pool.query('select name,' +
+      'address from author limit $1 offset $2',
+      [limit, offset], (err, res) => {
+        if (err) {
+          throw err;
+        }
+        console.log(res.rows);
+        resolve(res.rows);
+      });
+    }
+    );
   }
   console.log(data);
   return data;
