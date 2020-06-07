6499206603effb6fccdd88d36f5b5d91b121e66b
Daniil Denisyuk <ordekonec@gmail.com>
2020-06-07 16:46:55 +0300

    some changes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 const assert = require('assert').strict;
 const LoadBalancer = require('../lib/loadBalancer');
+const http = require('http');
 const Server = require('../lib/server');
 const Config = require('../lib/config');
 
@@ -10,6 +11,18 @@ const Config = require('../lib/config');
   const server1 = new Server(1, cnf);
   const server2 = new Server(2, cnf);
   const server3 = new Server(3, cnf);
+  const serverUrl = new URL('/api/authors', 'http://127.0.0.1:8082');
+  http.get(serverUrl, resp => {
+    let data = '';
+    resp.on('data', chunk => {
+      data += chunk;
+    });
+    resp.on('end', () => {
+      console.log(data);
+    });
+  }).on('error', err => {
+    console.log('Error: ' + err.message);
+  });
 })();
 //console.dir(cnf.sections);
 
